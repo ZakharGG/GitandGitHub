faf46e1a878b892c48c05f9b52f11e54451e2c6e
ZakharGG <zakhar.gatsinzi@icloud.com>
2020-11-12 18:01:15 -0500

    hallo group14

---
+++ src/Zakhar/java.java
@@ -0,0 +1,8 @@
+package Zakhar;
+
+public class java {
+    public static void main(String[] args) {
+
+    }
+    // hello group 14
+}
--- src/hallo.java
+++ src/hallo.java
@@ -1,5 +1,5 @@
 public class hallo {
     public static void main(String[] args) {
-        
+// jgoierjge
     }
 }
